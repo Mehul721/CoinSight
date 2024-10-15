9242ef9ed66d0e488483199e25d7c280bee5e32a
Mehul721 <jmehul721@gmail.com>
2024-10-15 23:41:41 +0530

    Initial Commit

---
+++ CoinSight/CoinSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinSightApp.swift
+//  CoinSight
+//
+//  Created by Mehul Jain on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
